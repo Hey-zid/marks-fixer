c31bcc98b9098241fe9164bcfbbe48498e9c1133
Hey-zid <git config --global user.email>
2025-07-18 16:24:10 +0600

    first commit

---
+++ 40 of us.py
@@ -0,0 +1,16 @@
+with open ("40 of us.txt","r") as lines:
+    lines_ = [line.strip().split(",") for line in lines]
+
+the_id=int(input("Please give the student ID  "))
+marks=int(input("Updated marks  "))
+
+for i in range(len(lines_)):
+    student_id = int(lines_[i][0])
+    if student_id == the_id:
+        lines_[i][2] = str(marks)
+
+with open("40 of us.txt", "w") as file:
+    for line in lines_:
+        file.write(",".join(line) + "\n")
+
+print("The Marks updated, Please check your file :)")
